e5717705ddc43b411e8fae8d0076ffa913151a24
Abdelrahmanmemara <119069265+Abdelrahmanmemara@users.noreply.github.com>
2024-06-18 01:06:43 +0200

    Trying to model the probability using the rate of change of LV

--- project.py
+++ project.py
@@ -9,15 +9,16 @@ import scipy.stats
 @deserialize
 @dataclass
 class LotkaVolterraConfig(Config):
-    fox_death_rate: float = 0.001
-    fox_reproduction_rate: float = 0.1
+    fox_death_rate: float = 0.01
+    fox_reproduction_rate: float = 0.01
     movement_speed: float = 1.0
     rabbit_reproduction_rate: float = 19/26 # Normalized the 19/6 probability of a rabbit reproducing
     average_life: float = 0.6 # In average 0.6 rabbits only stay alive in the litter.
     interaction_distance: float = 5.0  # Increase interaction distance
-
-foxes:int = 5
-rabbits:int = 20
+    fox_eat_rabbit_prob: float = 0.02  # Rate at which rabbits are eaten by foxes
+    dt: float= 0.1
+    foxes:int = 5
+    rabbits:int = 20
 
 class Fox(Agent):
     config: LotkaVolterraConfig
@@ -25,8 +26,9 @@ class Fox(Agent):
 
     def update(self):
         self.change_position()
-        if np.random.rand() < self.config.fox_death_rate:
+        if np.random.rand() < self.fox_pop_rate_of_change(self.config.rabbits,self.config.foxes,self.config.fox_reproduction_rate,self.config.fox_death_rate) * self.config.dt:
             self.kill()
+            self.config.foxes -= 1
         self.hunt()
     
     def hunt(self):
@@ -37,8 +39,14 @@ class Fox(Agent):
             if self.pos.distance_to(rabbit.pos) < self.config.interaction_distance:
                 print(f"Fox at {self.pos} hunting rabbit at {rabbit.pos}")  # Debugging statement
                 rabbit.kill()
+                self.config.rabbits -= 1
                 self.reproduce()
+                self.config.foxes += 1
                 break
+
+    def fox_pop_rate_of_change(self,B,F,c,d):
+        # Using the formula from the Lotka-Volterra
+        return (c*B*F) - (d*F)
 
 class Rabbit(Agent):
     config: LotkaVolterraConfig
@@ -51,6 +59,7 @@ class Rabbit(Agent):
         if self.D % 1000 == 0:
             if np.random.rand() < self.config.rabbit_reproduction_rate:
                 self.asexual_reproduction()
+                self.config.rabbits += 1
 
     def asexual_reproduction(self):
         # The rabbit can give birth up to 12 per litters.
@@ -60,18 +69,15 @@ class Rabbit(Agent):
                 self.reproduce()
 
 
-
-
-(
-    Simulation(
+simulation =  Simulation(
         LotkaVolterraConfig(
             image_rotation=True,
             movement_speed=1.0,
             radius=50,
             seed=1,
-        )
-    )
-    .batch_spawn_agents(5, Fox, images=["images/red.png"])
-    .batch_spawn_agents(20, Rabbit, images=["images/white.png"])
-    .run()
-)
+        ))
+
+simulation.batch_spawn_agents(5, Fox, images=["images/red.png"])
+simulation.batch_spawn_agents(20, Rabbit, images=["images/white.png"])
+simulation.run()
+
